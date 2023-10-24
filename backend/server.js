ba49a26f775d08726101c0c2c28f86c52da75bc8
Sahan Caldera <sahancaldera3@gmail.com>
2023-10-24 20:54:17 +0530

    Add Search by parameters

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require('express')
 const cors = require('cors')
-const elasticSearch = require('elasticsearch');
+const elasticSearch = require('elasticsearch')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
 const indexName = 'metaphoem'
 
@@ -24,8 +24,8 @@ app.get('/getAllQueries', (req, res) => {
         body: {
             size: 300,
             query:{
-                "match": {
-                    "metaphor_count": "1"
+                match: {
+                    metaphor_count: "1"
                 },
             },
         },
@@ -52,7 +52,7 @@ app.post('/poemNameSearch', async (req, res) => {
     }).then((response) => {
         res.json(response.hits.hits)
     }).catch((error) => {
-        res.status(500).send('Error retrieving target data')
+        res.sendStatus(500)
     })
 })
 
@@ -93,6 +93,66 @@ app.post('/targetSearch', async (req, res) => {
         res.json(response.hits.hits)
     }).catch((error) => {
         res.status(500).send('Error retrieving target data')
+    })
+})
+
+app.get("/searchQuery", async (req, res) => {
+    const poemName = await req.query.poem_name
+    const sourceDomain = await req.query.source_domain
+    const targetDomain = await req.query.target_domain
+
+    console.log(req.query)
+
+    let searchQueryBody = {
+        index: indexName,
+        body: {
+            size: 300,
+            query: {
+                bool: {
+                    must: [],
+                },
+            },
+        },
+    }
+
+    if (poemName) {
+        searchQueryBody.body.query.bool.must.push({
+            match: {
+                poem_name: poemName
+            }
+        })
+    }
+
+    if (sourceDomain) {
+        searchQueryBody.body.query.bool.must.push({
+            match: {
+                source_domain: sourceDomain
+            }
+        })
+    }
+
+    if (targetDomain) {
+        searchQueryBody.body.query.bool.must.push({
+            match: {
+                target_domain: targetDomain
+            }
+        })
+    }
+
+    // if (!poemName && !sourceDomain && !targetDomain) {
+    if (searchQueryBody.body.query.bool.must.length == 0) {
+        
+        searchQueryBody.body.query = {
+            match: {
+                metaphor_count: "1"
+            },
+        }
+    }
+    console.log(searchQueryBody.json)
+    client.search(searchQueryBody).then((response) => {
+        res.json(response.hits.hits)
+    }).catch((error) => {
+        res.status(500).send(error)
     })
 })
 
